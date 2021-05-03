335f242e31d0ab26da1e7eca94b15ed373da2b01
Manh Tuan <tuanbk189@gmail.com>
2021-05-03 22:12:08 +0700

    99. Resource Updating Endpoints: Part II

--- src/index.js
+++ src/index.js
@@ -53,7 +53,6 @@ app.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    console.log(req.params.id)
     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
     if (!user) {
@@ -99,6 +98,29 @@ app.get('/tasks/:id', async (req, res) => {
   } catch (e) {
     res.status(500).send(e)
   }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['description', 'completed'];
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+
 })
 
 app.listen(port, () => {
