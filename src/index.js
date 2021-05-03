72ea553224cc5d348042b9d0af0542e0863709d7
Manh Tuan <tuanbk189@gmail.com>
2021-05-03 20:38:59 +0700

    92. Resource Reading Endpoints: Part II

--- src/index.js
+++ src/index.js
@@ -28,18 +28,15 @@ app.get('/users', (req, res) => {
 
 app.get('/users/:id', (req, res) => {
   const _id = req.params.id;
-  console.log(_id)
 
   User.findById(_id).then((user) => {
     if (!user) {
       return res.status(404).send()
     }
-
     res.send(user)
   }).catch((e) => {
     res.status(500).send(e);
   })
-
 })
 
 app.post('/tasks', (req, res) => {
@@ -49,6 +46,28 @@ app.post('/tasks', (req, res) => {
     res.status(201).send(task)
   }).catch((e) => {
     res.status(400).send(e)
+  })
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks)
+  }).catch((e) => {
+    res.status(500).send(e)
+  })
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id).then((task) => {
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  }).catch((e) => {
+    res.status(500).send(e)
   })
 })
 
