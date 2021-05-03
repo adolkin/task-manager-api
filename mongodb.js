0badb06b9ef073c018786f23fa7ac9450bd13f38
Manh Tuan <tuanbk189@gmail.com>
2021-05-03 14:50:43 +0700

    81. Deleting Documents

--- mongodb.js
+++ mongodb.js
@@ -12,25 +12,18 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  // db.collection('users').updateOne({ _id: new ObjectID("608f77276678f00380f49136") }, {
-  //   $inc: {
-  //     age: 1
-  //   }
+  // db.collection('users').deleteMany({
+  //   age: 27
   // }).then((result) => {
   //   console.log(result)
   // }).catch((error) => {
   //   console.log(error)
   // })
 
-  db.collection('tasks').updateMany({
-    completed: false
-  }, {
-    $set: {
-      completed: true
-    }
-  }).then((result) => {
-    console.log(result.modifiedCount)
-  }).catch((error) => {
-    console.log(error)
-  })
+  db.collection('tasks').deleteOne({ _id: new ObjectID("608f9bd130718b4b78e3ca48") })
+    .then((result) => {
+      console.log(result)
+    }).catch((error) => {
+      console.log(error)
+    })
 })
