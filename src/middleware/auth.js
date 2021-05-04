0ffed10f6dc91153a703c4c26726d3b4394505fe
Manh Tuan <tuanbk189@gmail.com>
2021-05-04 22:20:20 +0700

    111. Logging Out

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,6 @@ const auth = async (req, res, next) => {
   try {
 
     const token = req.header('Authorization').replace('Bearer ', '')
-    console.log(token)
     const decoded = jwt.verify(token, 'thisismynewcourse')
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
@@ -13,6 +12,7 @@ const auth = async (req, res, next) => {
       throw new Error()
     }
 
+    req.token = token
     req.user = user
     next()
   } catch (e) {
