679bd1649e5891cbda262e97f7f8cf75162ca490
Manh Tuan <tuanbk189@gmail.com>
2021-05-08 09:22:40 +0700

    114. The User/Task Relationship

--- src/index.js
+++ src/index.js
@@ -13,3 +13,19 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // const task = await Task.findById('6095f347e34f96478442deeb')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+
+  const user = await User.findById('6095f0aa51a8c124e8b3f1e1')
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+
+}
+
+main()
